45e375b1ac4e7866c30c1746f61cd66ccfc454db
daemon369 <daemon336699@gmail.com>
2019-03-11 14:15:56 +0800

    use view background to show how color changes

--- app/src/main/java/me/daemon/colorpicker/demo/MainActivity.java
+++ app/src/main/java/me/daemon/colorpicker/demo/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onColor(int color) {
             Log.e("MainActivity", "onColor: " + color + " " + String.format("#%06X", (0xFFFFFF & color)));
+            findViewById(R.id.container).setBackgroundColor(colorPickerView.getColor());
         }
     };
 }
