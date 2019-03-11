b89d2960f2d4f31f973ad723cee6983d2977e155
daemon369 <daemon336699@gmail.com>
2019-03-11 08:25:30 +0800

    add alpha view

---
+++ colorpickerview/src/main/kotlin/me/daemon/colorpicker/view/AlphaView.kt
@@ -0,0 +1,15 @@
+package me.daemon.colorpicker.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author daemon
+ * @since 2019-03-11 08:24
+ */
+class AlphaView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
