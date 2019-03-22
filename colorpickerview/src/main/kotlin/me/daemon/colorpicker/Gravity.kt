2fcbb8cc426d5baafad9560c3cdb9919d8f9357e
daemon369 <daemon336699@gmail.com>
2019-03-22 16:25:22 +0800

    change Gravity constructors

--- colorpickerview/src/main/kotlin/me/daemon/colorpicker/Gravity.kt
+++ colorpickerview/src/main/kotlin/me/daemon/colorpicker/Gravity.kt
@@ -4,7 +4,7 @@ package me.daemon.colorpicker
  * @author daemon
  * @since 2019-02-22 00:00
  */
-enum class Gravity(val value: Int) {
+enum class Gravity {
     UNKNOWN(0),
 
     // basic
@@ -16,15 +16,52 @@ enum class Gravity(val value: Int) {
     CENTER_VERTICAL(CENTER_HORIZONTAL.value shl 1),
 
     // combination
-    LEFT_TOP(LEFT.value or TOP.value),
-    LEFT_CENTER(LEFT.value or CENTER_VERTICAL.value),
-    LEFT_BOTTOM(LEFT.value or BOTTOM.value),
-    CENTER_TOP(CENTER_HORIZONTAL.value or TOP.value),
-    CENTER(CENTER_HORIZONTAL.value or CENTER_VERTICAL.value),
-    CENTER_BOTTOM(CENTER_HORIZONTAL.value or BOTTOM.value),
-    RIGHT_TOP(RIGHT.value or TOP.value),
-    RIGHT_CENTER(RIGHT.value or CENTER_VERTICAL.value),
-    RIGHT_BOTTOM(RIGHT.value or BOTTOM.value);
+    LEFT_TOP(
+            LEFT,
+            TOP
+    ),
+    LEFT_CENTER(
+            LEFT,
+            CENTER_VERTICAL
+    ),
+    LEFT_BOTTOM(
+            LEFT,
+            BOTTOM
+    ),
+    CENTER_TOP(
+            CENTER_HORIZONTAL,
+            TOP
+    ),
+    CENTER(
+            CENTER_HORIZONTAL,
+            CENTER_VERTICAL
+    ),
+    CENTER_BOTTOM(
+            CENTER_HORIZONTAL,
+            BOTTOM
+    ),
+    RIGHT_TOP(
+            RIGHT,
+            TOP
+    ),
+    RIGHT_CENTER(
+            RIGHT,
+            CENTER_VERTICAL
+    ),
+    RIGHT_BOTTOM(
+            RIGHT,
+            BOTTOM
+    );
+
+    val value: Int
+
+    constructor(value: Int) {
+        this.value = value
+    }
+
+    constructor(horizontal: Gravity, vertical: Gravity) {
+        this.value = horizontal.value or vertical.value
+    }
 
     companion object {
         private val map = HashMap<Int, Gravity>()
